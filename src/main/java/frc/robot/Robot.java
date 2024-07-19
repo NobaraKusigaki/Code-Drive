9980104def3d42a656ec823fbcfba8d2bad4a95a
Macieira <esthermacieira1@gmail.com>
2024-07-18 23:44:47 -0300

    Update on robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,43 +5,32 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The VM is configured to automatically run this class, and to call the methods corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot
 {
     private Command autonomousCommand;
     
     private RobotContainer robotContainer;
-    
-    
+
     /**
      * This method is run when the robot is first started up and should be used for any
      * initialization code.
      */
     @Override
-    public void robotInit()
-    {
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        // autonomous chooser on the dashboard.
+    public void robotInit() {
         robotContainer = new RobotContainer();
+        for(int port = 5800; port <= 5809; port++){
+            PortForwarder.add(port, "limelight.local", port);
+        }
     }
     
     
-    /**
-     * This method is called every 20 ms, no matter the mode. Use this for items like diagnostics
-     * that you want ran during disabled, autonomous, teleoperated and test.
-     *
-     * <p>This runs after the mode specific periodic methods, but before LiveWindow and
-     * SmartDashboard integrated updating.
-     */
+
     @Override
     public void robotPeriodic()
     {
